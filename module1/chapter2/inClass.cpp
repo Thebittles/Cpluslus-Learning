fefb7c74fd853541d4f4c21056db47b0ce3093a8
Brittany <brittsdevlog@gmail.com>
2022-08-30 12:05:53 -0500

    Updating

---
+++ module1/chapter2/inClass.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "My name is: Brittany Sifford\n" ;
+    cout << "My age is: " << 29 << endl;
+    cout << "My height is: " << 5 << " feet and " <<  10 << "inches." << endl;
+    cout << "My gpa is: " << 2.82 << endl;
+    cout << "My middle initial is: " << 'R';
+
+    return 0;
+}
+
+
---
+++ module1/chapter2/practice.cpp
@@ -0,0 +1,12 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+   cout << 5 % 2;
+
+    return 0;
+}
